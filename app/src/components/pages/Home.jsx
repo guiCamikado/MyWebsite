95807ae6dbc45b0763e74f1a3e0f8e81adb973ad
guiCamikado <guilherme.camikado@gmail.com>
2025-12-21 12:53:21 -0300

    project Setup

---
+++ app/src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    /** Docs
+     * @startDate 21/12/25
+     * @lastUpdate 21/12/25 
+     * 
+     * @use Essa página é a homepage tem como função:
+     *      1. Oferecer uma página funcional de navegação após o login
+     *      2. Redirecionar caso usuário esteja conectado e link não ir a lugar nenhum 
+     */
+
+
+
+}
