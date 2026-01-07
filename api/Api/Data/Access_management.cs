fe5c22ae48b660e53ee259af4658fb503f3221f4
guiCamikado <guilherme.camikado@gmail.com>
2026-01-07 14:56:53 -0300

    Configurações da API funcionando

--- api/Api/Data/Access_management.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Data.Access_management
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Ip_addresss { get; set; }
-        public string Access_time { get; set; }
-    }
-}
