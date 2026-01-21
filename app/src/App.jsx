f62784e7b5ea8aae5646290a23332a9c5e0694e8
guiCamikado <guilherme.camikado@gmail.com>
2026-01-21 01:27:10 -0300

    Fix commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,5 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/pages/home'
-
-//Temp
-import RegisterPage from './components/temp/RegisterPage'
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -10,7 +7,6 @@ function App() {
     // Quando se utiliza HashRouter o Router passa a reconhecer após o caractere # então utiliza-se #link
     <HashRouter>
       <Routes>
-        <Route path="/test" element={<RegisterPage />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </HashRouter>
